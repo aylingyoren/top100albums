23803f234e7536121f9500b6b1498f8591fa7733
Aylin Gyoren <aylingyoren21@gmail.com>
2022-03-22 21:59:27 +0300

    style: style SearchBar compt

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { BsSearch } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
+import "./SearchBar.css";
 
-function SearchBar() {
-  return <h5>SearchBar</h5>;
+function SearchBar({ term, setTerm }) {
+  const clearInput = () => setTerm("");
+  return (
+    <div className="searchbar">
+      <input
+        className="searchbar__input"
+        value={term}
+        placeholder="Search Albums..."
+        onChange={(e) => setTerm(e.target.value)}
+      />
+      {term.length === 0 ? (
+        <BsSearch
+          role="button"
+          tabIndex={0}
+          size="24px"
+          className="searchbar__magnifier"
+        />
+      ) : (
+        <AiOutlineClose
+          role="button"
+          tabIndex={0}
+          size="30px"
+          className="searchbar__close"
+          onClick={clearInput}
+        />
+      )}
+    </div>
+  );
 }
 
 export default SearchBar;
