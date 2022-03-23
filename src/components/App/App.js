8facd66b6d5f29726d8351a6afd3281a9db72281
Aylin Gyoren <aylingyoren21@gmail.com>
2022-03-23 11:49:45 +0300

    feat: create n pass FavoritesContext

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,34 @@
 import { Routes, Route } from "react-router-dom";
+import { FavoritesContext } from "../../context/FavoritesContext";
+import { useFavorites } from "../../hooks/useFavorites";
 import HomePage from "../HomePage/HomePage";
 import NavBar from "../NavBar/NavBar";
 import SearchPage from "../SearchPage/SearchPage";
 import "./App.css";
 
 function App() {
+  const { favorites, addToFavorites, removeFromFavorites, setFavorites } =
+    useFavorites();
+
   return (
     <div className="app">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="search" element={<SearchPage />} />
-        <Route
-          path="*"
-          element={
-            <div style={{ padding: "1rem" }}>
-              <h1>Error 404: Not Found</h1>
-            </div>
-          }
-        />
-      </Routes>
+      <FavoritesContext.Provider
+        value={[favorites, addToFavorites, removeFromFavorites, setFavorites]}
+      >
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route
+            path="*"
+            element={
+              <div style={{ padding: "1rem" }}>
+                <h1>Error 404: Not Found</h1>
+              </div>
+            }
+          />
+        </Routes>
+      </FavoritesContext.Provider>
     </div>
   );
 }
---
+++ src/context/FavoritesContext.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+export const FavoritesContext = createContext([
+  [],
+  () => null,
+  () => null,
+  () => null,
+]);
