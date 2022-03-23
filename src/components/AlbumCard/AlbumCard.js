836637346acf6c25327aa8edbde1c7d41e0cdfb9
Aylin Gyoren <aylingyoren21@gmail.com>
2022-03-23 10:37:26 +0300

    feat: fetch n display data wd styling

--- src/components/AlbumCard/AlbumCard.js
+++ src/components/AlbumCard/AlbumCard.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./AlbumCard.css";
 
-function AlbumCard() {
-  return <h6>AlbumCard</h6>;
-}
+const AlbumCard = ({ img, title, singer }) => (
+  <div className="albumcard">
+    <img className="albumcard__img" src={img} alt="Album thumbnail" />
+    <h4 className="albumcard__title">{title}</h4>
+    <h6 className="albumcard__singer">{singer}</h6>
+  </div>
+);
 
 export default AlbumCard;
--- src/components/AlbumsList/AlbumsList.js
+++ src/components/AlbumsList/AlbumsList.js
@@ -1,12 +1,24 @@
 import React from "react";
 import AlbumCard from "../AlbumCard/AlbumCard";
+import "./AlbumsList.css";
+import { useFetch } from "../hooks/useFetch";
 
 function AlbumsList() {
+  const { data, loading, error } = useFetch(
+    "https://itunes.apple.com/us/rss/topalbums/limit=100/json"
+  );
+
   return (
-    <>
-      <h5>AlbumsList</h5>
-      <AlbumCard />
-    </>
+    <div className="albumslist">
+      {data.map((album, i) => (
+        <AlbumCard
+          key={`album-item-${i}`}
+          title={album["im:name"].label}
+          img={album["im:image"][2].label}
+          singer={album["im:artist"].label}
+        />
+      ))}
+    </div>
   );
 }
 
