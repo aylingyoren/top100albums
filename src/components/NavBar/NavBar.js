9c2515e80ca70e69fb0798d57cb0777bf19ab66e
Aylin Gyoren <aylingyoren21@gmail.com>
2022-03-23 18:02:02 +0300

    feat: add active navbar link state

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "./../../assets/img/ailin_designed_logo.png";
 import "./NavBar.css";
 
 function NavBar() {
+  const [navActive, setNavActive] = useState("home");
+
   return (
     <div className="navbar">
       <Link to="/">
         <img src={Logo} alt="logo" className="navbar__logo" />
       </Link>
       <Link to="/">
-        <div className="navbar__link">Home</div>
+        <div
+          className={
+            navActive === "home" ? "navbar__link-active" : "navbar__link"
+          }
+          onClick={() => setNavActive("home")}
+        >
+          Home
+        </div>
       </Link>
       <Link to="topalbums">
-        <div className="navbar__link">Search</div>
+        <div
+          className={
+            navActive === "search" ? "navbar__link-active" : "navbar__link"
+          }
+          onClick={() => setNavActive("search")}
+        >
+          Search
+        </div>
       </Link>
       <Link to="favorites">
-        <div className="navbar__link">Favorites</div>
+        <div
+          className={
+            navActive === "favorites" ? "navbar__link-active" : "navbar__link"
+          }
+          onClick={() => setNavActive("favorites")}
+        >
+          Favorites
+        </div>
       </Link>
     </div>
   );
