a5436463d86c7f957a0c16229cde3839f9570c67
Aylin Gyoren <aylingyoren21@gmail.com>
2022-03-23 19:14:13 +0300

    feat: add favorite icon

--- src/components/AlbumCard/AlbumCard.js
+++ src/components/AlbumCard/AlbumCard.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { FavoritesContext } from "../../context/FavoritesContext";
+import { BsSuitHeart, BsSuitHeartFill } from "react-icons/bs";
 import "./AlbumCard.css";
 
 const AlbumCard = ({ album }) => {
@@ -12,22 +13,25 @@ const AlbumCard = ({ album }) => {
 
   return (
     <div className="albumcard">
-      <img className="albumcard__img" src={albumImageSrc} alt={albumTitle} />
+      <div className="albumcard__img_group">
+        <img className="albumcard__img" src={albumImageSrc} alt={albumTitle} />
+        <div
+          className="albumcard__heart"
+          onClick={() =>
+            favorites?.find((el) => el["im:name"].label === albumTitle)?.isFav
+              ? removeFromFavorites(album, albumTitle)
+              : addToFavorites(album)
+          }
+        >
+          {favorites.find((el) => el["im:name"].label === albumTitle) ? (
+            <BsSuitHeartFill size="40px" />
+          ) : (
+            <BsSuitHeart size="40px" />
+          )}
+        </div>
+      </div>
       <h4 className="albumcard__title">{albumTitle}</h4>
       <h6 className="albumcard__singer">{albumSinger}</h6>
-      <button
-        type="button"
-        className="albumcard__btn"
-        onClick={() =>
-          favorites?.find((el) => el["im:name"].label === albumTitle)?.isFav
-            ? removeFromFavorites(album, albumTitle)
-            : addToFavorites(album)
-        }
-      >
-        {favorites.find((el) => el["im:name"].label === albumTitle)
-          ? "Remove Favorite"
-          : "Add Favorite"}
-      </button>
     </div>
   );
 };
