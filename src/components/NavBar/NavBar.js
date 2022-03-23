bb931429e7725bf78f0e80fe1b75643988c6fe43
Aylin Gyoren <aylingyoren21@gmail.com>
2022-03-23 23:41:02 +0300

    refactor: add link mapping in navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,44 +3,42 @@ import { Link } from "react-router-dom";
 import Logo from "./../../assets/img/ailin_designed_logo.png";
 import "./NavBar.css";
 
+const links = [
+  {
+    url: "/",
+    title: "Home",
+  },
+  {
+    url: "/topalbums",
+    title: "Top 100",
+  },
+  {
+    url: "/favorites",
+    title: "Favorites",
+  },
+];
+
 function NavBar() {
-  const [navActive, setNavActive] = useState("home");
+  const [navActive, setNavActive] = useState("Home");
 
   return (
     <div className="navbar">
-      <Link to="/">
+      <Link to="/" onClick={() => setNavActive("Home")}>
         <img src={Logo} alt="logo" className="navbar__logo" />
       </Link>
-      <Link to="/">
-        <div
+
+      {links.map(({ url, title }, i) => (
+        <Link
+          key={`link-${i}`}
+          onClick={() => setNavActive(title)}
+          to={url}
           className={
-            navActive === "home" ? "navbar__link-active" : "navbar__link"
+            navActive === title ? "navbar__link-active" : "navbar__link"
           }
-          onClick={() => setNavActive("home")}
         >
-          Home
-        </div>
-      </Link>
-      <Link to="topalbums">
-        <div
-          className={
-            navActive === "search" ? "navbar__link-active" : "navbar__link"
-          }
-          onClick={() => setNavActive("search")}
-        >
-          Search
-        </div>
-      </Link>
-      <Link to="favorites">
-        <div
-          className={
-            navActive === "favorites" ? "navbar__link-active" : "navbar__link"
-          }
-          onClick={() => setNavActive("favorites")}
-        >
-          Favorites
-        </div>
-      </Link>
+          {title}
+        </Link>
+      ))}
     </div>
   );
 }
